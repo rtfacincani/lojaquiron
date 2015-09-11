6b431ceb01d6d214bf34b8f399157887610e59e8
rtfacincani <ricardo.facincani@gmail.com>
2015-09-11 18:30:38 -0300

    Criação das tabelas iniciais do DB

---
+++ QuironDatabase/Produto.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Produto]
+(
+	[ProdutoId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NVARCHAR(150) NULL, 
+    [Descricao] NVARCHAR(500) NULL, 
+    [Categoria] NVARCHAR(50) NULL, 
+    [Preco] DECIMAL(16, 2) NULL
+)
