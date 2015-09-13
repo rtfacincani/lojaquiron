535ced29d1a89cd9b02004b5e284e555e14fa233
rtfacincani <ricardo.facincani@gmail.com>
2015-09-13 13:08:55 -0300

    Teste no macbook

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -10,8 +10,8 @@ namespace Quiron.LojaVirtual.Dominio.Entidade
     {
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public decimal Preco { get; set; }
+        public string Descricao { get; set; }  
+        public decimal Preco { get; set; } 
         public string Categoria { get; set; }
 
     }
